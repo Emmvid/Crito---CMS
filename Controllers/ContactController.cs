2cde9031251c2f867eb0ac31aa9632be10474137
Emmvid <emmavidarsson@hotmail.com>
2023-10-06 11:19:31 +0200

    Added hardcoded parts on homepage

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -7,6 +7,7 @@ using Umbraco.Cms.Core.Web;
 using Umbraco.Cms.Infrastructure.Persistence;
 using Umbraco.Cms.Web.Website.Controllers;
 using Crito.Models;
+using Crito.Services;
 
 namespace Crito.Controllers
 {
@@ -20,9 +21,14 @@ namespace Crito.Controllers
         public IActionResult Index(ContactForm contactForm)
         {
             if (!ModelState.IsValid)
-            {
                 return CurrentUmbracoPage();
-            }
+
+            using var mail = new MailService("no-reply@crito.com", "smtp.crito.se", 587, "contactform@crito.com", "Bytmig123!"); ;
+
+            //to sender
+            mail.SendAsync(contactForm.Email, "Your Message was recieved", "Hi, thank you for your email. We will contact you soon").ConfigureAwait(false);
+            // to reciever   
+            mail.SendAsync("support@crito.com", $"{contactForm.Name} sent you a message", contactForm.Message).ConfigureAwait(false);
 
 
             return LocalRedirect(contactForm.RedirectUrl ?? "/");
